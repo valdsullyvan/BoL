bd9977edf0fb995100ad99f2465c8e897d6907a0
spyk <swan524@users.noreply.github.com>
2015-06-04 12:50:44 +0200

    Update KogMawk.lua

--- kog/KogMawk.lua
+++ kog/KogMawk.lua
@@ -18,6 +18,7 @@
 		  Updated Stacking for not VIP_USERS
 		  Updated Scripts for targeting all users, free and vip.
 		  Removed skin hack from TAB and code because it's too fat to load and to useless.. We don't need a skin to carry, but, if you want a skin go with MK Jogo/Mk LoL, it's a skin hack usable for all champions without script and that's legal..
+		  Cleared the code and refresh some broken function..
 		  ]]--
 
 local charNames = {
