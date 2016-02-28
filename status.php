376de47140a569f95d72043696616a31cd89e553
spyk <spyk1@users.noreply.github.com>
2016-02-28 03:09:47 +0100

    Status.php personal website

---
+++ status.php
@@ -0,0 +1,80 @@
+<?php 
+
+	$actuelle_version = "6.4";
+	$ancienne_version = "6.3";
+
+?>
+
+<html>
+	<head>
+	
+	  <style>
+      body {
+      	background-color: #2c3e50;
+      	color: #ffffff;
+      	font-family: verdana;
+      }
+      
+      h3 {
+      	margin-left: 1%;
+      }
+      
+      a {
+      	text-decoration: none;
+      	color: #ffffff;
+      	margin-left: 5%;
+      }
+      
+      .BoL {
+      	font-family: gabriola;
+      }
+    </style>
+
+		<LINK rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+		<LINK rel="icon" type="image/png" href="http://img15.hostingpics.net/pics/176762favicon.png" />
+
+		<META http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+		<title>spyk | LUA | PROJETS | STATUS </title>
+
+	</head>
+
+	<body>
+
+		<center><i class="fa fa-flask fa-5x"></i></center>
+
+		<br /><br />
+
+		<h1><center>Actual League version : <FONT color="#FEFEE2"><u><?php echo $actuelle_version; ?></u></font> <br />(Next update for the XX/XX)</center></h1>
+
+		<br /><br />
+
+		<h2><center>Script up-to-date:</center></h2>
+
+			<br />
+			<h3>Champion Related.</h3>
+				<br /><a href="Kalista.php">[Beta]  Baguette   Kalista      ➔ <FONT color="#27ae60"><?php echo $actuelle_version; ?></font></a><br />
+				<a href="Anivia.php">[Beta]  Baguette   Anivia       ➔ <FONT color="#27ae60"><?php echo $actuelle_version; ?></font></a><br />
+				<a href="Malzahar.php">[Beta]  Baguette   Malzahar     ➔ <FONT color="#27ae60"><?php echo $actuelle_version; ?></font></a><br />
+				<a href="Sona.php">[Beta]  Baguette   Sona         ➔ <FONT color="#27ae60"><?php echo $actuelle_version; ?></font></a><br />
+
+			<h3>Usable Related Projects.</h3>
+				<a href="Potions.php">[Lite]  Baguette   Potions      ➔ <FONT color="#27ae60"><?php echo $actuelle_version; ?></font></a><br />
+
+		<br />
+
+		<h2><center>Script broken:</center></h2>
+		<br />
+	</body>
+
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-73030373-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+</html>
