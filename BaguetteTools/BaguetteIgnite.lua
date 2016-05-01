92b488141537ddf6e42adb5e768d59b188322444
spyk <spyk1@users.noreply.github.com>
2016-05-01 18:41:53 +0200

    removed auto update because not needed

--- BaguetteTools/BaguetteIgnite.lua
+++ BaguetteTools/BaguetteIgnite.lua
@@ -1,32 +1,4 @@
 local LastMSG = 0
---- Starting AutoUpdate.
-local version = "0.1"
-local author = "spyk"
-local SCRIPT_NAME = "BaguetteIgnite"
-local AUTOUPDATE = true
-local UPDATE_HOST = "raw.githubusercontent.com"
-local UPDATE_PATH = "/spyk1/BoL/master/BaguetteTools/BaguetteIgnite.lua".."?rand="..math.random(1,10000)
-local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
-local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
-
-if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/spyk1/BoL/master/BaguetteTools/BaguetteIgnite.version")
-	if ServerData then
-		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
-		if ServerVersion then
-			if tonumber(version) < ServerVersion then
-				EnvoiMessage("New version available "..ServerVersion)
-				EnvoiMessage(">>Updating, please don't press F9<<")
-				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () EnvoiMessage("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
-			else
-				DelayAction(function() EnvoiMessage("Hello, "..GetUser()..". You got the latest version! :) ("..ServerVersion..") Enjoy you'r game!") end, 1)
-			end
-		end
-		else
-			EnvoiMessage("Error downloading version info")
-	end
-end
- --- End Of AutoUpdate.
 
 function EnvoiMessage(msg)
 	PrintChat("<font color=\"#e74c3c\"><b>[BaguetteIgnite]</b></font> <font color=\"#ffffff\">" .. msg .. "</font>")
