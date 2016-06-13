7627730eaf7362a7391277f8c1d1f3fa60acc308
spyk <spyk1@users.noreply.github.com>
2016-06-13 07:42:47 +0200

    Create callback.lua

---
+++ BaguetteTools/dev/callback.lua
@@ -0,0 +1,14 @@
+AddDrawCallback(function() self:OnDraw() end);
+AddTickCallback(function() self:OnTick() end);
+AddLoadCallback(function() self:OnLoad() end);
+AddUnloadCallback(function() self:Unload() end);
+AddNewPathCallback(function(unit, startPos, endPos, isDash ,dashSpeed,dashGravity, dashDistance) self:OnNewPath(unit, startPos, endPos, isDash, dashSpeed, dashGravity, dashDistance) end);
+AddAnimationCallback(function(unit, action) self:Animation(unit, action) end);
+AddProcessSpellCallback(function(unit, spell) self:OnProcessSpell(unit, spell) end);
+AddProcessAttackCallback(function(unit, spell) self:OnProcessAttack(unit, spell) end)
+AddMsgCallback(function(msg,key) self:OnWndMsg(msg,key) end);
+AddSendPacketCallback(function(p) self:OnSendPacket(p) end);
+AddRecvPacketCallback(function(p) self:OnRecvPacket(p) end);
+AddCreateObjCallback(function(object) end);
+AddDeleteObjCallback(function(object) end);
+AddChatCallback(function(msg, prefix) self:OnSendChat(msg, prefix) end);
