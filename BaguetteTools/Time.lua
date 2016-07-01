712d4d06e761f7536c172f5ce8a305d360d6664a
spyk <spyk1@users.noreply.github.com>
2016-07-01 21:03:02 +0200

    Update Time.lua

--- BaguetteTools/Time.lua
+++ BaguetteTools/Time.lua
@@ -1,3 +1,3 @@
 AddDrawCallback(function()
-  DrawText(os.date("%A, %B %d %Y - %X - "..GetLatency().." ms"), 15, 1340, 10, 0xFFFFFFFF);
-end)
+  DrawText(os.date("%A, %B %d %Y - %X - "..GetLatency().." ms"), 15, WINDOW_W/1.45, WINDOW_W/180, 0xFFFFFFFF);
+end);
